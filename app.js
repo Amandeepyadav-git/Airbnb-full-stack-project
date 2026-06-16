3263fcf80816488c333508b4ba407f12e2499c3e
Amandeep Yadav <amandeepyadav0704@gmail.com>
2026-06-16 23:14:34 +0530

    error phase all working properly

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.all(/.*/, (req, res, next)=>{
 //middleware for error handling
 app.use((err, req, res, next)=>{
   let {statusCode = 500, message = "Something went wrong!"} = err;
-  res.render("error.ejs", {message});
+  res.render("error.ejs", { message });
   // res.status(statusCode).send(message);
 })
 
